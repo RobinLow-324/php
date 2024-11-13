22c7456597c01e211d6fc66d9d412a4f6783ebc3
Robin Low <robinlow0324@e.neivce.edu.my>
2024-11-13 14:48:55 +0800

    add date time ,color using span and font size

--- Week2/index.php
+++ Week2/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
     <style>
-        body {background-color: black;}
+        body { background-color: blue; }
+        .text1 { color: aqua; font-size: 100px; }
+        .text2 { color: red; font-size: 50px; }
+        .text3 { color: orange; font-size: 50px; }
     </style>
-    </head>
-    <body>
-        <?php echo "My first PHP script!"; echo date("1")?>
-    </body>
+</head>
+<body>
+    <?php
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    echo "<span class='text1'>Robin Low Jian Bin</span>";
+    ?>
+    <br>
+    <span class="text2"><?php echo date("d F Y"); ?></span>
+    <br>
+    <span class="text3"><?php echo date("g : i A"); ?></span>
+</body>
 </html>
