2799ff07d1258eee3f7a54ed1ee8e606de3242ac
Robin Low <robinlow0324@e.neivce.edu.my>
2024-11-13 13:28:29 +0800

    change name

---
+++ sedsc017.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+
+            My PHP
+            
+        </title>
+    </head>
+    <body>
+        <?php echo "Robin"; ?>
+    </body>
+</html>
